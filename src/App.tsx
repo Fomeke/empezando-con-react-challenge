6a7d0cd8eac44add27af99b65eea431a2b254dbc
fomeke <seba.navarrozales@gmail.com>
2026-06-02 20:30:24 -0400

    agregar texto cambiante con boton

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
+
 import("./App.css");
 
 function App() {
+  const [esNegrita, setEsNegrita] = useState(false);
+
   return (
     <div className="contenedor">
-      <h1 className="texto">Hola mundo</h1>
+      <h1 className={`texto ${esNegrita ? "bold" : "normal"}`}>Hola mundo</h1>
+      <button onClick={() => setEsNegrita(!esNegrita)}>Dame click</button>
     </div>
   );
 }
