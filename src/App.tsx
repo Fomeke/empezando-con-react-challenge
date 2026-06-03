5c20aff4434653e1f30039ba1f412261fd52e46e
fomeke <seba.navarrozales@gmail.com>
2026-06-02 21:00:50 -0400

    texto con cambio de color y fondo con cambio de color

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,25 @@ import { useState } from "react";
 import("./App.css");
 
 function App() {
-  const [esNegrita, setEsNegrita] = useState(false);
+  const [isBlack, setIsBlack] = useState(false);
+
+  const [colorText, setColorText] = useState("#000000");
+  const [colorFund, setColorFund] = useState("#ffffff");
+
+  useState("Arial");
 
   return (
-    <div className="contenedor">
-      <h1 className={`texto ${esNegrita ? "bold" : "normal"}`}>Hola mundo</h1>
-      <button onClick={() => setEsNegrita(!esNegrita)}>Dame click</button>
+    <div className="container" style={{ backgroundColor: colorFund }}>
+      <h1
+        className={`text ${isBlack ? "bold" : "normal"}`}
+        style={{ color: colorText }}
+      >
+        Hola mundo
+      </h1>
+      <button onClick={() => setIsBlack(!isBlack)}>Dame click</button>
+      <input onChange={(e) => setColorText(e.target.value)} type="color" />
+
+      <input onChange={(e) => setColorFund(e.target.value)} type="color" />
     </div>
   );
 }
