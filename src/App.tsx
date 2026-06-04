9618d7f1687469edc2ae9df345c0692f42d8fb07
fomeke <seba.navarrozales@gmail.com>
2026-06-03 20:53:00 -0400

    boton de edicion y todas los demas requisitos funcionales

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,51 @@ import("./App.css");
 function App() {
   const [isBlack, setIsBlack] = useState(false);
 
+  const [text] = useState("Hello world");
   const [colorText, setColorText] = useState("#000000");
   const [colorFund, setColorFund] = useState("#ffffff");
-
-  useState("Arial");
+  const [nameFont, setNameFont] = useState("Dancing Script");
+  const [view, setView] = useState(false);
+  const [temporalText, setTemporalText] = useState(text);
 
   return (
     <div className="container" style={{ backgroundColor: colorFund }}>
       <h1
-        className={`text ${isBlack ? "bold" : "normal"}`}
-        style={{ color: colorText }}
+        className={`${text} ${isBlack ? "bold" : "normal"}`}
+        style={{ color: colorText, fontFamily: nameFont }}
       >
-        Hola mundo
+        {temporalText}
       </h1>
-      <button onClick={() => setIsBlack(!isBlack)}>Dame click</button>
+      <button onClick={() => setIsBlack(!isBlack)}>Click me</button>
       <input onChange={(e) => setColorText(e.target.value)} type="color" />
-
       <input onChange={(e) => setColorFund(e.target.value)} type="color" />
+      <select onChange={(e) => setNameFont(e.target.value)}>
+        <option value="Dancing Script">Dancing Script</option>
+        <option value="Orbitron">Orbitron</option>
+        <option value="Ole">Ole</option>
+        <option value="Playwrite GB J">Playwrite England Joined</option>
+        <option value="Black Ops One">Black Ops One</option>
+      </select>
+      <button className="pencil" onClick={() => setView(true)}>
+        ✏️
+      </button>
+      {view && (
+        <div className="modal">
+          <input
+            onChange={(e) => setTemporalText(e.target.value)}
+            type="text"
+          />
+          <button onClick={() => setView(false)}>Cancel</button>
+          <button
+            onClick={() => {
+              setTemporalText(temporalText);
+              setView(false);
+            }}
+          >
+            Accept
+          </button>
+        </div>
+      )}
     </div>
   );
 }
